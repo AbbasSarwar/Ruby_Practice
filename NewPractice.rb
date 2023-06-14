875ae4303a9ec948151856ae923f455ca4a348f3
Abbas Sarwar <abbassarwar100@gmail.com>
2023-06-14 17:54:00 +0500

    Ruby Basic Practices

---
+++ NewPractice.rb
@@ -0,0 +1,24 @@
+puts 2+2
+puts 10/3.0
+puts "Happy" + "coding"
+puts "Happy" + "\n" + "coding"
+puts "Happy" + "\n" + "coding" + "\n"
+
+a = 3
+puts a
+a = "Hello"
+puts a
+b = " World!"
+puts (a + b)
+
+def hello(name)
+    puts ("Hello #{name}!")
+end
+
+:hello
+hello('abbas')
+
+def addition(a, b)
+    puts a + b
+end
+addition(1,3)
